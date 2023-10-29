c554f7c2bd5d24d733c166b0b5964efef4dd86b5
JunlinWang <12112921@mail.sustech.edu.cn>
2023-10-29 22:52:05 +0800

    Update demo.sh

--- homework1/demo.sh
+++ homework1/demo.sh
@@ -5,38 +5,44 @@ c. Display whether a file is a \"simple\" file or a \"directory\"\n
 d. Create a backup for a file\n
 e. Start an ftp session\n
 f. Start your LED control program\n
-x. Exit\n
-Please input a character to invoke an option: " 
+x. Exit" 
 
-read option
+option='s' 
+while [ $option != 'x' ]
+do
+    
+    case $option in
+        a)
+            ls -al
+            ;;
+        b)
+            date
+            ;;
+        c)
+            echo "Please input a filename: "
+            read filename
+            if [ -d $filename ]; then
+                echo $filename" is a directory"
+            else
+                echo $filename" is a simple file"
+            fi
+            ;;
+        d)
+            echo "Please input a filename: "
+            read filename
+            cp $filename "backup_"$filename
+            ;;
+        e)
+            echo "Please input a domain name or an IP address: "
+            read ip
+            ftp $ip
+            ;;
+        f)
+            python draft.py
+            ;;
+        esac
 
-case $option in
-    a)
-        ls -al
-        ;;
-    b)
-        date
-        ;;
-    c)
-        echo "Please input a filename: "
-        read filename
-        if [ -d $filename ]; then
-            echo $filename" is a directory"
-        else
-            echo $filename" is a simple file"
-        fi
-        ;;
-    d)
-        echo "Please input a filename: "
-        read filename
-        cp $filename "backup_"$filename
-        ;;
-    e)
-        ftp
-        ;;
-    f)
-        python draft.py
-        ;;
-    x)
-        ;;
-    esac
+    echo "Please input a character to invoke an option: "
+    read option
+
+done
